511213cb9bbd6dfa6d3a0be258659d5c248d22b8
Piyush Shah <sahp4552@gmail.com>
2026-07-04 16:11:20 +0530

    Update backend URL to Render

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // Aapka backend port 4000 par chal raha hai
-const BACKEND_URL = 'http://localhost:4000';
+const BACKEND_URL = "https://collabodraw-api.onrender.com";
 
 // 'autoConnect: false' rakha hai taaki hum manually connect karein jab canvas load ho
 export const socket = io(BACKEND_URL, {
